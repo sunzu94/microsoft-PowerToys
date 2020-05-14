4a36179ebff308d2c82c1035cc9d1fa39e7c07c9
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-14 11:10:53 -0400

    Initial implementation of GifEncoder

--- src/modules/gifmaker/AnimatedGifRecorder/Encoders/GifEncoder.cs
+++ src/modules/gifmaker/AnimatedGifRecorder/Encoders/GifEncoder.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using Microsoft.Win32;
 
 namespace AnimatedGifRecorder.Encoders
 {
+    /// <summary>
+    /// Taken from https://docs.microsoft.com/en-us/dotnet/framework/wpf/graphics-multimedia/how-to-encode-and-decode-a-gif-image
+    /// </summary>
     public static class GifEncoder
     {
         /// <summary>
@@ -16,7 +21,19 @@ namespace AnimatedGifRecorder.Encoders
         /// <param name="filePath"></param>
         public static void Encode(List<FrameInfo> frames, string filePath)
         {
-            throw new NotImplementedException();
+            FileStream stream = new FileStream(filePath, FileMode.Create);
+            GifBitmapEncoder encoder = new GifBitmapEncoder();
+            
+            foreach(var frame in frames)
+            {
+                // Open a Stream and decode a PNG image
+                Stream imageStreamSource = new FileStream(frame.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                PngBitmapDecoder decoder = new PngBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                BitmapSource bitmapSource = decoder.Frames[0];
+
+                encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+            }
+            encoder.Save(stream);
         }
     }
 }
