81494e853949fab20eff545bf1ece9fe7236080f
Nancy Zhao <32007540+zhaonancy@users.noreply.github.com>
2020-05-14 14:06:37 -0400

    porting over resizable rectangle

--- src/modules/gifmaker/AnimatedGifRecorder/Views/RecordArea.xaml.cs
+++ src/modules/gifmaker/AnimatedGifRecorder/Views/RecordArea.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimatedGifRecorder.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,8 @@ using System.Windows.Shapes;
 namespace AnimatedGifRecorder.Views
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for RecordArea.xaml
+    /// Resizable Rectangle from: https://github.com/kareemsulthan07/ResizableRectangleSample
     /// </summary>
     public partial class RecordArea : UserControl
     {
@@ -25,19 +27,66 @@ namespace AnimatedGifRecorder.Views
             InitializeComponent();
         }
 
-        private void MainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        private ResizableRectangle rect;
+        private void Area_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            try
+            {
+                Area.CaptureMouse(e.Pointer);
+                var ptrPoint = e.GetPosition(Area);
+                var point1 = ptrPoint.Position;
 
+                Area.Children.OfType<ResizableRectangle>()
+                    .ToList()
+                    .ForEach(r => r.IsSelected = false);
+
+                rect = new ResizableRectangle(Area, point1);
+                Area.Children.Add(rect);
+
+                e.Handled = true;
+            }
+            catch (Exception)
+            {
+            }
         }
 
-        private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
+        private void Area_MouseMove(object sender, MouseEventArgs e)
         {
-
+            try
+            {
+                var ptrPoint = e.GetPosition(Area);
+                if (ptrPoint.Properties.IsLeftButtonPressed)
+                {
+                    if (rect != null)
+                    {
+                        var point2 = ptrPoint.Position;
+                        rect.SetCoordinates(point2);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
         }
 
-        private void MainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Area_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+            try
+            {
+                Area.ReleaseMouseCapture();
 
+                if (rect.IsZeroSize())
+                {
+                    Area.Children.Remove(rect);
+                }
+
+                rect = null;
+
+                e.Handled = true;
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
