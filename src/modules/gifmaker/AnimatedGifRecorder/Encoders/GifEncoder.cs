6113f501cfd12c203a798f1008108fbcdebe819b
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-14 10:56:00 -0400

    Declared GIF Encoder class

---
+++ src/modules/gifmaker/AnimatedGifRecorder/Encoders/GifEncoder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace AnimatedGifRecorder.Encoders
+{
+    public static class GifEncoder
+    {
+        /// <summary>
+        /// Takes a sequence of frameinfos and merges them into a single GIF
+        /// </summary>
+        /// <param name="frames"></param>
+        /// <param name="filePath"></param>
+        public static void Encode(List<FrameInfo> frames, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
